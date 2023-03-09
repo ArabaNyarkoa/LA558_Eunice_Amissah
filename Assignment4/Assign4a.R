064b189ee70fcd452065ffd177a3184ff1350dbe
ArabaNyarkoa <eunice@iastate.edu>
2023-03-08 20:53:11 -0600

    assign4

---
+++ Assignment4/Assign4a.R
@@ -0,0 +1,99 @@
+# Eunice Amissah-Mensah
+# 
+# Assignment 4_A
+# Leaflet mapping: adding multiple markers to slippy map
+
+install.packages("leaflet", "leaflet.providers", "tidyverse")
+library(leaflet)
+library(leaflet.providers)
+library(tidyverse)
+
+
+CarRally100 <- read.csv("CarRally100.csv", header = TRUE)
+CarRally1000 <- read.csv("CarRally1000.csv", header = TRUE)
+CarData <- CarRally100
+
+# Add markers from the CSV to this map
+map <- leaflet(CarData) %>% 
+  addTiles() %>%
+  addMarkers(~longitude, ~latitude)
+map
+
+# Circles might be better for this map, while at it maybe also include labels
+# a typical syntax for this is label = ~htmlEscape(Name) however we won't escape
+# instead I use the name of the data set, myData followed by a $ and var name
+map <- leaflet(CarData) %>% 
+  addTiles() %>%
+  addCircles(~longitude, ~latitude, label = CarData$car_make)
+map
+
+# Popup of the people's car model year
+
+map <- leaflet(CarData) %>% 
+  addTiles() %>%
+  addCircles(~longitude, ~latitude, popup = paste("<strong>", 
+                                                  CarData$first_name, "</strong><br>", "Car Model Year: ", 
+                                                  CarData$car_year))
+map
+
+# Change the size and color of the circles
+
+map <- leaflet(CarData) %>% 
+  addTiles() %>%
+  addCircles(~longitude, ~latitude, popup= paste("<strong>", 
+                                                 CarData$first_name, "</strong><br>", "Car Model Year: ", 
+                                                 CarData$car_year), weight = 8, radius=1000, 
+             color="purple", stroke = TRUE, fillOpacity = 1.0)
+map
+
+?addCircles
+
+# Base map options
+
+names(providers)
+
+# Button containing a group of background layers
+map <- leaflet(CarData) %>% 
+  addTiles(group = "OSM", options = providerTileOptions(minZoom = 4, maxZoom = 10)) %>%
+  addProviderTiles("Stamen.Terrain", group = "Terrain", 
+                   options = providerTileOptions(minZoom = 8, maxZoom = 10)) %>%
+  addProviderTiles("Stamen.Watercolor", group = "Water Color") %>%
+  addProviderTiles("Esri.WorldStreetMap", group = "Street") %>%
+  addProviderTiles("OpenStreetMap.Mapnik", group = "Mapnik") %>%
+  addProviderTiles("Thunderforest.Landscape", group = "Landscape") %>%
+  addLayersControl(baseGroups = c("OSM", "Terrain", "Water Color", "Street", "Mapnik", "Landscape"),
+                   options = layersControlOptions(collapsed = TRUE)) %>%
+  addCircles(~longitude, ~latitude, popup= paste("<strong>", 
+                                                 CarData$first_name, "</strong><br>", "Shirt Size: ", 
+                                                 CarData$car_year), weight = 8, radius=40, 
+             color="purple", stroke = TRUE, fillOpacity = 1.0)
+map
+
+# Add markers from the CSV with 1000 to this map. 
+CarData <-CarRally1000
+map <- leaflet(CarData) %>% 
+  addProviderTiles("Stamen.TonerLite", 
+                   options = providerTileOptions(minZoom = 4, maxZoom = 10)) %>%
+  addMarkers(~longitude, ~latitude)
+map
+
+# Display first 200 observations
+map <- leaflet(CarData[1:200,]) %>% 
+  addProviderTiles("Stamen.TonerLite", 
+                   options = providerTileOptions(minZoom = 4, maxZoom = 10))%>%
+  addMarkers(~longitude, ~latitude)
+map
+
+# Make clusters
+map <- leaflet(CarData[1:200,]) %>% 
+  addProviderTiles("Stamen.TonerLite", 
+                   options = providerTileOptions(minZoom = 4, maxZoom = 10))%>%
+  addMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions())
+map
+
+# One more try on all 1,000 locations
+map <- leaflet(CarData) %>% 
+  addProviderTiles("Stamen.TonerLite", 
+                   options = providerTileOptions(minZoom = 4, maxZoom = 10))%>%
+  addMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions())
+map
---
+++ Assignment4/Assign4b.R
@@ -0,0 +1,188 @@
+# Eunice Amissah-Mensah
+#
+# Assignment 4_B 
+# Leaflet mapping: Make a Chloropleth map.
+
+
+install.packages("leaflet", "leaflet.providers", "tidyverse", "sf")
+library(leaflet)
+library(leaflet.providers)
+library(tidyverse)
+library(readxl)
+library(sf)
+
+# Set working directory to the same as this R file.
+# Read in the shapefile
+carCount<- st_read("TXCarRally.shp")
+
+# Set the projection to use lat and longs
+carCount <- st_transform(carCount, crs = 4326)
+
+carCount <- carCount %>% rename(count = first_name)
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount,  
+              color = "purple", fill = NA, weight = 2)
+txmap
+
+# Display only a few counties,
+carCount_selection1 <- carCount %>% 
+  filter(CNTY_NM %in% c("Harris", "Uvalde", "Fayette"))
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount_selection1,  
+              color = "#800080", fillColor = "yellow", weight = 2, opacity = 0.5)
+txmap
+
+carCount_selection2 <- carCount %>% 
+  filter(is.na(count) | !count > 0)
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount_selection2,  
+              color = "#000", fillColor = "darkgreen", weight = 1, 
+              opacity = 0.75, fillOpacity = 0.8)
+txmap
+
+
+# Both NA and Selected counties
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount_selection2,  
+              color = "#000", fillColor = "darkgreen", weight = 1, 
+              opacity = 0.75, fillOpacity = 0.8) %>%
+  
+  addPolygons(data = carCount_selection1,  
+              color = "#800080", fillColor = "yellow", weight = 5, 
+              opacity = 0.75, fillOpacity = 0.8)
+txmap
+
+# NA to 0
+carCount <- carCount %>%
+  replace(is.na(.), 0)
+
+# Select the color scheme from Color Brewer
+library("RColorBrewer") #I think either Leaflet or tidyverse loads this for you
+display.brewer.all()
+
+
+bins <- c(0, 4, 8, 12, 16, Inf)
+pal <- colorBin("BuPu", domain = carCount$count, bins = bins)
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount,
+              fillColor = ~pal(count),
+              weight = 0.5,
+              opacity = 1,
+              color = "grey",
+              dashArray = "1",
+              fillOpacity = 0.8)
+txmap
+
+
+# Add interaction
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount,
+              fillColor = ~pal(count),
+              weight = 0.5,
+              opacity = 1,
+              color = "grey",
+              dashArray = "1",
+              fillOpacity = 0.8,  #be careful, you need to switch the ) to a comma
+              highlightOptions = highlightOptions(
+                weight = 2,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE)
+  )
+txmap
+
+# Lablels
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g cars",
+  carCount$CNTY_NM, carCount$count
+) %>% lapply(htmltools::HTML)
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 6)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount,
+              fillColor = ~pal(count),
+              weight = 0.5,
+              opacity = 1,
+              color = "grey",
+              dashArray = "1",
+              fillOpacity = 0.8,  
+              highlightOptions = highlightOptions(
+                weight = 2,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labels,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "10px",
+                direction = "auto"))
+txmap
+
+
+# Add a legend
+txmap %>% addLegend(pal = pal, values = count, opacity = 0.7, title = "Cars",
+                position = "bottomright")
+
+
+# ----------------Complete Code!!  ------
+
+
+carCount <- st_read("TXCarRally.shp")
+carCount <- st_transform(carCount, crs = 4326)
+carCount <- carCount %>% rename(count = first_name)
+carCount <- carCount %>% replace(is.na(.), 0)
+
+display.brewer.all() # I am selecting the Reds
+bins <- c(0, 4,8,12, 16, Inf)
+pal <- colorBin("BuPu", domain = carCount$count, bins = bins)
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g cars",
+  carCount$COUNTY, carCount$count
+) %>% lapply(htmltools::HTML)
+
+txmap <- leaflet() %>%
+  setView(-99.5, 32.2, 5)  %>%
+  addTiles() %>%
+  addPolygons(data = carCount,
+              fillColor = ~pal(count),
+              weight = 0.5,
+              opacity = 1,
+              color = "grey",
+              dashArray = "1",
+              fillOpacity = 0.8,  
+              highlightOptions = highlightOptions(
+                weight = 2,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labels,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "10px",
+                direction = "auto")) %>% 
+  addLegend(pal = pal, values = count, opacity = 0.7, title = "Cars",
+            position = "bottomright")
+txmap
+
+#now export this as a web page!!!
